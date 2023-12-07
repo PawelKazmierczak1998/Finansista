2b30031173a70a1956615201bd669c73b660501c
Paweł Kaźmierczak <102613700+PawelKazmierczak1998@users.noreply.github.com>
2023-12-07 16:33:20 +0100

    W

---
+++ Finansista/Finansista/Models/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using Finansista.Data.Enum;
+
+namespace Finansista.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public decimal amount { get; set; }
+        public string? description { get; set; }
+        public int balanceId { get; set; }
+        public virtual Balance? Balance { get; set; }
+        public TransactionType TransactionType { get; set; }
+        public DateTime? CreatedAt { get { return DateTime.Now; } }
+    }
+}
