5b01f3e13c781014ce4185a80bad30eec92d2765
Paweł Kaźmierczak <102613700+PawelKazmierczak1998@users.noreply.github.com>
2023-12-07 16:25:47 +0100

    Added Balance Model

--- Finansista/Finansista/Models/Balance.cs
+++ Finansista/Finansista/Models/Balance.cs
@@ -1,7 +1,11 @@
-﻿namespace Finansista.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Finansista.Models
 {
     public class Balance
     {
-
+        public int Id { get; set; }
+        public decimal accountBalance { get; set; }
+        public string accountName { get; set; }
     }
 }
